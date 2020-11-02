7fb7f53c70422edde03dfe3a001f07570f36e4a7
Argo-Cloud <argo@163.com>
2020-11-03 00:51:35 +0800

    feat: 增加 src 源码同步脚本

---
+++ scripts/sync.cmd
@@ -0,0 +1,21 @@
+@echo off
+if "%1" == "" (
+    echo Please provider SolutionDir like: "$(MSBuildThisFileDirectory)"
+    exit /B
+)
+
+set sourceDir=%1..\template-ui\src
+set targetDir=%1src
+
+echo "Ready to copy files to dist
+
+echo xcopy %sourceDir%\*.* %targetDir% /y
+
+rd /Q /S %sourceDir%\.vs
+rd /Q /S %sourceDir%\BootstrapBlazorApp.Server\bin
+rd /Q /S %sourceDir%\BootstrapBlazorApp.Server\obj
+rd /Q /S %sourceDir%\BootstrapBlazorApp.WebAssembly\bin
+rd /Q /S %sourceDir%\BootstrapBlazorApp.WebAssembly\obj
+xcopy %sourceDir%\*.* %targetDir% /E /R /Y
+
+echo Sync success!
