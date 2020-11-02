538df4d35ebfc14cfaec2f650061e06f6c4e09de
Argo-Cloud <argo@163.com>
2020-11-03 01:06:12 +0800

    scripts: 增加 linux 脚本

---
+++ sync.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ $1 = "" ]; then
+    echo Please provider SolutionDir like: "$(MSBuildThisFileDirectory)"
+    exit 1
+fi
+
+sourceDir=$1../template-ui/src
+targetDir=$1src
+
+echo "Ready to copy files to dist"
+
+echo copy $sourceDir $targetDir 
+
+rm /fr %sourceDir%/.vs
+rm /fr %sourceDir%/BootstrapBlazorApp.Server/bin
+rm /fr %sourceDir%/BootstrapBlazorApp.Server/obj
+rm /fr %sourceDir%/BootstrapBlazorApp.WebAssembly/bin
+rm /fr %sourceDir%/BootstrapBlazorApp.WebAssembly/obj
+\cp -rf $sourceDir $targetDir
+
+echo Sync success!
