500a3d83912b871c2151a8c17010fa16eee5e529
Argo-Unicom <argo@163.com>
2021-02-01 01:17:05 +0800

    chore: 更新 sync 脚本

--- scripts/sync.cmd
+++ scripts/sync.cmd
@@ -4,17 +4,15 @@ if "%1" == "" (
     exit /B
 )
 
-set sourceDir=%1..\template-ui\src
+set sourceDir=%1..\src
 set targetDir=%1src
 
 echo "Ready to copy files to dist
 
-echo xcopy %sourceDir%\*.* %targetDir% /y
-
 rd /Q /S %targetDir%\BootstrapBlazorApp.Server
 rd /Q /S %targetDir%\BootstrapBlazorApp.WebAssembly
 rd /Q /S %targetDir%\BootstrapBlazorApp.Shared
-del %targetDir%\BootstrapBlazorApp.sln
+
 rd /Q /S %sourceDir%\.vs
 rd /Q /S %sourceDir%\BootstrapBlazorApp.Server\bin
 rd /Q /S %sourceDir%\BootstrapBlazorApp.Server\obj
@@ -22,6 +20,7 @@ rd /Q /S %sourceDir%\BootstrapBlazorApp.WebAssembly\bin
 rd /Q /S %sourceDir%\BootstrapBlazorApp.WebAssembly\obj
 rd /Q /S %sourceDir%\BootstrapBlazorApp.Shared\bin
 rd /Q /S %sourceDir%\BootstrapBlazorApp.Shared\obj
+
 xcopy %sourceDir%\*.* %targetDir% /E /R /Y
 
 echo Sync success!
