17b64e5258fb22d78f39093d19ce0e9cc672139f
Argo-Tianyi <argo@163.com>
2021-11-21 16:25:18 +0800

    refactor: 使用 net6.0 模板

--- template-ui/BootstrapBlazorApp.WebAssembly/Program.cs
+++ template-ui/BootstrapBlazorApp.WebAssembly/Program.cs
@@ -1,46 +1,27 @@
 ﻿using BootstrapBlazorApp.Shared;
 using BootstrapBlazorApp.Shared.Data;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace BootstrapBlazorApp.WebAssembly
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// 增加 BootstrapBlazor 组件
+builder.Services.AddBootstrapBlazor(op =>
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public class Program
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    // 设置组件默认使用中文
+    op.DefaultCultureInfo = "zh";
+});
 
-            builder.RootComponents.Add<App>("app");
+builder.Services.AddSingleton<WeatherForecastService>();
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// 增加 Table 数据服务操作类
+builder.Services.AddTableDemoDataService();
 
-            // 增加 BootstrapBlazor 组件
-            builder.Services.AddBootstrapBlazor(op =>
-            {
-                // 设置组件默认使用中文
-                op.DefaultCultureInfo = "zh";
-            });
-
-            builder.Services.AddSingleton<WeatherForecastService>();
-
-            // 增加 Table 数据服务操作类
-            builder.Services.AddTableDemoDataService();
-
-            var host = builder.Build();
-
-            await host.RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
