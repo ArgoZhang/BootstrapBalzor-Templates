b25e53a918d626cf1011fcd373ea48d765a0e263
Argo-Lenovo <argo@163.com>
2021-11-25 17:04:14 +0800

    chore: wasm 使用默认模板

--- template-ui/BootstrapBlazorApp.WebAssembly/Program.cs
+++ template-ui/BootstrapBlazorApp.WebAssembly/Program.cs
@@ -7,7 +7,7 @@ using System;
 using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
