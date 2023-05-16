d72283ca09ee935b90cbda8f0ea7cc6e7e2315dd
Super毛毛虫 <2629580846@qq.com>
2023-05-16 12:59:21 +0000

    !1 项目模板支持Server only模式和WebAssembly only模式 * 补充提交 * VISX项目模板Tag增加balzor * VISX项目模板Tag增加balzor

--- scripts/sync.cmd
+++ scripts/sync.cmd
@@ -7,6 +7,9 @@ if "%1" == "" (
 set sourceDir=%1..\src
 set targetDir=%1src
 
+set expansionSourceDir=%1..\src\expansions
+set expansionTargetDir=%1expansions\
+
 echo "Ready to copy files to dist
 
 rd /Q /S %targetDir%\BootstrapBlazorApp.Server
@@ -21,6 +24,16 @@ rd /Q /S %sourceDir%\BootstrapBlazorApp.WebAssembly\obj
 rd /Q /S %sourceDir%\BootstrapBlazorApp.Shared\bin
 rd /Q /S %sourceDir%\BootstrapBlazorApp.Shared\obj
 
+
+rd /Q /S %expansionTargetDir%\BootstrapBlazorApp.OnlyServer /XD .template.config
+rd /Q /S %expansionTargetDir%\BootstrapBlazorApp.OnlyWasm /XD .template.config
+
+rd /Q /S %expansionSourceDir%\BootstrapBlazorApp.OnlyServer\bin
+rd /Q /S %expansionSourceDir%\BootstrapBlazorApp.OnlyServer\obj
+rd /Q /S %expansionSourceDir%\BootstrapBlazorApp.OnlyWasm\bin
+rd /Q /S %expansionSourceDir%\BootstrapBlazorApp.OnlyWasm\obj
+
+
 mkdir %targetDir%\BootstrapBlazorApp.Server
 xcopy %sourceDir%\BootstrapBlazorApp.Server %targetDir%\BootstrapBlazorApp.Server /S /R /Y
 
@@ -31,5 +44,11 @@ mkdir %targetDir%\BootstrapBlazorApp.Shared
 xcopy %sourceDir%\BootstrapBlazorApp.Shared %targetDir%\BootstrapBlazorApp.Shared /S /R /Y
 
 xcopy %sourceDir%\..\src\*.sln %targetDir% /Y
+
+mkdir %expansionTargetDir%\BootstrapBlazorApp.OnlyServer
+xcopy %expansionSourceDir%\BootstrapBlazorApp.OnlyServer %expansionTargetDir%\BootstrapBlazorApp.OnlyServer /S /R /Y
+
+mkdir %expansionTargetDir%\BootstrapBlazorApp.OnlyWasm
+xcopy %expansionSourceDir%\BootstrapBlazorApp.OnlyWasm %expansionTargetDir%\BootstrapBlazorApp.OnlyWasm /S /R /Y
 
 echo Sync success!
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -10,6 +10,9 @@ fi
 sourceDir=$1../src
 targetDir=$1
 
+expansionSourceDir=$1..\src\expansions
+expansionTargetDir=$1expansions\
+
 echo "Ready to copy files to dist"
 
 echo copy $sourceDir $targetDir 
@@ -22,8 +25,18 @@ rm -rf $sourceDir/BootstrapBlazorApp.WebAssembly/bin
 rm -rf $sourceDir/BootstrapBlazorApp.WebAssembly/obj
 rm -rf $sourceDir/BootstrapBlazorApp.Shared/bin
 rm -rf $sourceDir/BootstrapBlazorApp.Shared/obj
+
+find $expansionTargetDir/expansions/BootstrapBlazorApp.OnlyServer -not -path $expansionTargetDir/expansions/BootstrapBlazorApp.OnlyServer/.template.config* -delete
+rm -rf $expansionSourceDir/BootstrapBlazorApp.OnlyServer/bin
+rm -rf $expansionSourceDir/BootstrapBlazorApp.OnlyServer/obj
+find $expansionTargetDir/expansions/BootstrapBlazorApp.OnlyWasm -not -path $expansionTargetDir/expansions/BootstrapBlazorApp.OnlyWasm/.template.config* -delete
+rm -rf $expansionSourceDir/BootstrapBlazorApp.OnlyWasm/bin
+rm -rf $expansionSourceDir/BootstrapBlazorApp.OnlyWasm/obj
+
 \cp -rf $sourceDir $targetDir
 
 \cp $sourceDir/../src/*.sln $targetDir
+
+\cp -rf $expansionSourceDir $expansionTargetDir
 
 echo Sync success!
