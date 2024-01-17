241d4447a5defa3f7da3fc4b00082a506b9e7452
Argo-AscioTech <argo@live.ca>
2024-01-17 10:43:49 +0800

    refactor: 更新 Sever 模板

---
+++ src/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using BootstrapBlazorApp.Server.Components;
+using BootstrapBlazorApp.Server.Data;
+using Microsoft.AspNetCore.SignalR;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddBootstrapBlazor();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// 增加 Table 数据服务操作类
+builder.Services.AddTableDemoDataService();
+
+// 增加 SignalR 服务数据传输大小限制配置
+builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseResponseCompression();
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
