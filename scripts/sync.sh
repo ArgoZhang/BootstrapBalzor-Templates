c074c38dace7512b276e4bc4bc22d9398f878dc8
Argo-MacAir <argo@live.ca>
2024-02-11 19:18:06 +0800

    chore: 更新打包脚本

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -17,9 +17,10 @@ echo "Ready to copy files to dist"
 
 echo copy $sourceDir $targetDir 
 
-rm -rf $targetDir/auto/BootstrapBlazorApp*
-rm -rf $targetDir/server/BootstrapBlazorApp*
-rm -rf $targetDir/webassembly/BootstrapBlazorApp*
+rm -rf $targetDir/auto/BootstrapBlazorApp
+rm -rf $targetDir/auto/BootstrapBlazorApp.Client
+rm -rf $targetDir/server/BootstrapBlazorApp.Server
+rm -rf $targetDir/webassembly/BootstrapBlazorApp.WebAssembly
 
 rm -rf $sourceDir/Auto/BootstrapBlazorApp/bin
 rm -rf $sourceDir/Auto/BootstrapBlazorApp/obj
@@ -32,13 +33,14 @@ rm -rf $sourceDir/Server/BootstrapBlazorApp.Server/obj
 rm -rf $sourceDir/WebAssembly/BootstrapBlazorApp.WebAssembly/bin
 rm -rf $sourceDir/WebAssembly/BootstrapBlazorApp.WebAssembly/obj
 
-\cp -rf $sourceDir/Auto/*.* $targetDir/auto
+\cp -rf $sourceDir/Auto/BootstrapBlazorApp/ $targetDir/auto/BootstrapBlazorApp/
+\cp -rf $sourceDir/Auto/BootstrapBlazorApp.Client/ $targetDir/auto/BootstrapBlazorApp.Client/
 \cp $sourceDir/.editorconfig $targetDir/auto
 
-\cp -rf $sourceDir/Server/*.* $targetDir/server
-\cp $sourceDir/.editorconfig $targetDir/server
+\cp -rf $sourceDir/Server/BootstrapBlazorApp.Server/ $targetDir/server/BootstrapBlazorApp.Server/
+\cp $sourceDir/.editorconfig $targetDir/server/
 
-\cp -rf $sourceDir/WebAssembly/*.* $targetDir/webassembly
-\cp $sourceDir/.editorconfig $targetDir/webassembly
+\cp -rf $sourceDir/WebAssembly/BootstrapBlazorApp.WebAssembly/ $targetDir/webassembly/BootstrapBlazorApp.WebAssembly/
+\cp $sourceDir/.editorconfig $targetDir/webassembly/
 
 echo Sync success!
