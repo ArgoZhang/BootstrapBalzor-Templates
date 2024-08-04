7c2e8222a184c2d568ac383764efc22bbef74ca1
Argo-AscioTech <argo@live.ca>
2024-08-04 19:04:10 +0800

    feat: 增加 Pdf 导出功能示例

--- src/Server/BootstrapBlazorApp.Server/Program.cs
+++ src/Server/BootstrapBlazorApp.Server/Program.cs
@@ -11,6 +11,12 @@ builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 builder.Services.AddBootstrapBlazor();
 
+// 增加 Pdf 导出服务
+builder.Services.AddBootstrapBlazorTableExportService();
+
+// 增加 Html2Pdf 服务
+builder.Services.AddBootstrapBlazorHtml2PdfService();
+
 // 增加 SignalR 服务数据传输大小限制配置
 builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
 
